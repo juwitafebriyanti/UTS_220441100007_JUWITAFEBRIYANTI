d2f0cc3b278a1778ec085a4fdf3d9ad23c0ce8a5
juwitafebriyanti <113221680+juwitafebriyanti@users.noreply.github.com>
2023-10-13 08:20:16 +0700

    Add files via upload

---
+++ UTS_220441100007_JUWITAFEBRIYANTI/src/main/java/uts/UTS_220441100007_JUWITAFEBRIYANTI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uts;
+
+/**
+ *
+ * @author juwit
+ */
+public class UTS_220441100007_JUWITAFEBRIYANTI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
